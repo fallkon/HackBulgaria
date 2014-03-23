34f3f39e4ae52e1f51d9e751ceec1f07f9714b67
oleg <o.trulla@gmail.com>
2014-03-24 01:30:39 +0200

    corection bags in tes for solution of nth_fibonacci

--- week_0/nth_fibonacci/test.py
+++ week_0/nth_fibonacci/test.py
@@ -6,7 +6,7 @@ class TestSolution(unittest.TestCase):
     """docstring for NthFibonacciTest"""
     def test_nth_fibonacci_0(self):
         self.assertEqual(1, nth_fibonacci(2))
-    def test_nth_fibonacci_1
+    def test_nth_fibonacci_1(self):
         self.assertEqual(55, nth_fibonacci(10))
 
 if __name__ == '__main__':
